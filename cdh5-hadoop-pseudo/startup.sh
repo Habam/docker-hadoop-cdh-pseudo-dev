c57267e032c56c9bef6916af7fad2c6f4a96aa39
bruceli <bruce.li@ellipsiz-comms.com>
2015-10-21 13:25:59 +0800

    Make startup.sh infinite running

--- cdh5-hadoop-pseudo/startup.sh
+++ cdh5-hadoop-pseudo/startup.sh
@@ -3,3 +3,6 @@ for x in `cd /etc/init.d ; ls hadoop-hdfs-*` ; do service $x start ; done
 service hadoop-yarn-resourcemanager start
 service hadoop-yarn-nodemanager start 
 service hadoop-mapreduce-historyserver start
+
+# for infinite foreground running
+tail -f /dev/null
