22336c96fe6c654b01d436a82d34c8b7c662230c
bruceli <bruce.li@ellipsiz-comms.com>
2015-10-22 12:44:52 +0800

    modify container layer sequence

--- cdh5-hadoop-pseudo-zk-hbase/startup-zk-hbase.sh
+++ cdh5-hadoop-pseudo-zk-hbase/startup-zk-hbase.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
+for x in `cd /etc/init.d ; ls hadoop-hdfs-*` ; do service $x start ; done
+service hadoop-yarn-resourcemanager start
+service hadoop-yarn-nodemanager start
+service hadoop-mapreduce-historyserver start
+
 service zookeeper-server start
 service hbase-master start
 service hbase-regionserver start
